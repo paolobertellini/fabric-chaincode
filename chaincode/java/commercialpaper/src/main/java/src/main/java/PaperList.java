ef492836e3f4b9802079243e8d6619291271b69d
Unimore <225184@studenti.unimore.it>
2021-05-14 10:09:15 +0200

    Custom context

---
+++ chaincode/java/commercialpaper/src/main/java/src/main/java/PaperList.java
@@ -0,0 +1,33 @@
+/*
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package org.example;
+
+import org.example.ledgerapi.StateList;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+public class PaperList {
+
+    private StateList stateList;
+
+    public PaperList(Context ctx) {
+        this.stateList = StateList.getStateList(ctx, PaperList.class.getSimpleName(), CommercialPaper::deserialize);
+    }
+
+    public PaperList addPaper(CommercialPaper paper) {
+        stateList.addState(paper);
+        return this;
+    }
+
+    public CommercialPaper getPaper(String paperKey) {
+        return (CommercialPaper) this.stateList.getState(paperKey);
+    }
+
+    public PaperList updatePaper(CommercialPaper paper) {
+        this.stateList.updateState(paper);
+        return this;
+    }
+}
